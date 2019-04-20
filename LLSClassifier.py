c2ad116d8277138dfcf3437cd4a1770c2c9abbc4
Ryan Horoff <rhoroff@gmail.com>
2019-04-20 16:32:59 -0400

    For Liam

--- LLSClassifier.py
+++ LLSClassifier.py
@@ -1,28 +1,39 @@
-import numpy as np 
+import numpy as np
 import numpy.linalg as la
 import csv
 import sys
 import os
 
 
-def parseDatabaseIntoMatrix(inputFile):
+def parse_database_into_matrix(inputFile):
+    # Full class path in case its necessary
     inputFile = os.path.abspath(inputFile)
     with open(inputFile) as csvfile:
         datareader = csv.reader(csvfile)
         data_point_matrix = []
         for row in datareader:
-            data_point =[]
+            data_point = []
             for i in range(len(row)):
                 data_point.append(row[i])
             data_point_matrix.append(data_point)
-        return([np.asarray(data_point_matrix), np.transpose(data_point_matrix)])
+        return np.asarray(data_point_matrix), np.asarray(data_point_matrix).T
+
+
+def compute_class_matrix(inputMatrix, classDiscriminatorColumn):
+    # Check if the descirminators are already in int form
+    if isinstance(inputMatrix[0][classDiscriminatorColumn], int):
+        for i in range(inputMatrix.shape):
+
+    else:
+
+
+
+def compute_weight_vector(inputMatrix, classMatrix):
+    return
+
 
 if __name__ == '__main__':
     inputFile = sys.argv[1]
-    dataset_and_tranpsose = parseDatabaseIntoMatrix(inputFile)
-    print(dataset_and_tranpsose[0])
-    print(dataset_and_tranpsose[1])
-
-
-
-            
+    X, X_t = parse_database_into_matrix(inputFile)
+    print(X)
+    print(X_t)
