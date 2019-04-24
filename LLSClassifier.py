92e38cc8a35fc2f0cc7931e36b729ba41c83794a
Ryan Horoff <rhoroff@gmail.com>
2019-04-24 12:00:44 -0400

    Fix: parsing into databases now produces data with datapoints as columns as opposed to rows

--- LLSClassifier.py
+++ LLSClassifier.py
@@ -1,20 +1,24 @@
 import numpy as np
 import numpy.linalg as la
-import csv, sys, os, re
+import csv
+import sys
+import os
+import re
+
 
 def classification_to_vector(classification):
     if classification == '1':
-        return [1,0,0]
+        return [1, 0, 0]
     elif classification == '2':
-        return [0,1,0]
+        return [0, 1, 0]
     elif classification == '3':
-        return [0,0,1]
+        return [0, 0, 1]
     elif classification == 'Iris-setosa':
-        return [1,0,0]
+        return [1, 0, 0]
     elif classification == 'Iris-virginica':
-        return [0,1,0]
+        return [0, 1, 0]
     elif classification == 'Iris-versicolor':
-        return [0,0,1]
+        return [0, 0, 1]
 
 
 def parse_database_into_matrix(inputFile):
@@ -36,20 +40,19 @@ def parse_database_into_matrix(inputFile):
                 classification = row[class_col]
 
                 # 'Iris-setosa' -> [1,0,0], 'Iris-virginica' -> [0,1,0], 'Iris-versicolor' -> [0,0,1]
-                classifications.append(classification_to_vector(classification))
+                classifications.append(
+                    classification_to_vector(classification))
                 for i in range(len(row)-1):
                     data_point.append(row[i])
 
                 data_point_matrix.append(data_point)
-            data_point_matrix = np.asarray(data_point_matrix, dtype=np.float32)
-            data_point_matrix = data_point_matrix.T
-            
-            ones = np.ones((1, data_point_matrix.shape[1]))
-            data_point_matrix = np.concatenate((data_point_matrix, ones))
-            data_point_matrix = data_point_matrix.T
-            print(data_point_matrix)
 
-            return data_point_matrix, classifications
+            #Append a 1 to every data point for the free param
+            data_point_matrix = np.asarray(data_point_matrix, dtype=np.float32)
+            ones = np.ones((data_point_matrix.shape[0], 1))
+            data_point_matrix = np.concatenate((data_point_matrix, ones),1)
+
+            return data_point_matrix.T, np.asarray(classifications).T
 
     elif re.search("wine.data", inputFile):
         with open(inputFile) as csvfile:
@@ -67,35 +70,37 @@ def parse_database_into_matrix(inputFile):
                 # '1' -> [1,0,0], '2' -> [0,1,0], '3' -> [0,0,1]
                 classifications.append(classification_to_vector(row[0]))
 
-                for i in range(1,len(row)):
+                for i in range(1, len(row)):
                     data_point.append(row[i])
 
                 data_point_matrix.append(data_point)
+            #Append a 1 to every data point for the free param
             data_point_matrix = np.asarray(data_point_matrix, dtype=np.float32)
-            data_point_matrix = data_point_matrix.T
-            
-            ones = np.ones((1, data_point_matrix.shape[1]))
-            data_point_matrix = np.concatenate((data_point_matrix, ones))
-            data_point_matrix = data_point_matrix.T
+            data_point_matrix = np.asarray(data_point_matrix, dtype=np.float32)
+            ones = np.ones((data_point_matrix.shape[0], 1))
+            data_point_matrix = np.concatenate((data_point_matrix, ones),1)
 
-            return data_point_matrix, np.asarray(classifications)
+            return data_point_matrix.T, np.asarray(classifications).T
     else:
-        return [],[]
+        return [], []
+
 
 def train_weight_vector(inputMatrix, classMatrix, inputLambda):
     X = np.asarray(inputMatrix)
-    X = X.T
     Y = np.asarray(classMatrix)
-    conditioning_lambda = inputLambda #Arbitrarily small value to start, will vary for testing purposes
-    W = np.dot(la.inv(np.dot(X,X.T) + conditioning_lambda), np.dot(X,Y))
-    calc_W = (np.asarray(la.lstsq(X.T, Y)[0]))
+    # Arbitrarily small value to start, will vary for testing purposes
+    conditioning_lambda = inputLambda
+    W = np.dot(la.inv(np.dot(X, X.T) + conditioning_lambda), np.dot(X, Y.T))
+    print(W)
+    print(la.lstsq(X.T,Y.T)[0])
 
     return W
 
-def classify(W,x_i):
-    return 1
+def split_data_into_training_and_testing(database, trainingPercentage):
+    return 0
+
 
 if __name__ == '__main__':
     inputFile = sys.argv[1]
     X, Y = parse_database_into_matrix(inputFile)
-    W = train_weight_vector(X,Y, .001) # arbitrary lambda for now
+    W = train_weight_vector(X, Y, .001)  # arbitrary lambda for now
