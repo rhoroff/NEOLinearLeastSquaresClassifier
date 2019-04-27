962f2391695e954d20a552f1cca2e32844997177
Liam Wood <liamvwood@ufl.edu>
2019-04-27 17:25:37 -0400

    added function structure

--- SVMClassifier.py
+++ SVMClassifier.py
@@ -1,15 +1,23 @@
 import numpy as np
 from sklearn.svm import SVC
 
-# Training data X -> features, Y -> labels
-X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
-Y = np.array([1, 1, 2, 2])
+def train_classifier(classifier, X, y):
+    # train the classifier with the data
+    classifier.fit(X, y)
+    return classifier
 
-# initialize the support vector class
-classifier = SVC(gamma='auto')
+def test_classifier(classifier, X):
+    # test with one or more data points
+    return classifier.predict(X)
 
-# train the classifier with the data
-classifier.fit(X,Y)
+if __name__ == '__main__':
+    # Training data X -> features, Y -> labels
+    X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1], [0,0]])
+    Y = np.array([1, 1, 2, 2, 3])
 
-# test with one or more data points
-print(classifier.predict(X))
+    # initialize the support vector class
+    classifier = SVC(gamma='auto')
+
+    classifier = train_classifier(classifier, X, Y)
+
+    print(test_classifier(classifier, [[1,0]]))
